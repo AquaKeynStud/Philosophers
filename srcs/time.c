9219ff894723c128975a6faa13881784d8ec712a
Keyn <arocca42student@gmail.com>
2025-08-21 13:06:23 +0200

    Update de la fonction de timer pour coller avec linux et le projet

--- srcs/time.c
+++ srcs/time.c
@@ -6,27 +6,20 @@
 /*   By: arocca <arocca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:21:38 by arocca            #+#    #+#             */
-/*   Updated: 2025/08/02 14:54:50 by arocca           ###   ########.fr       */
+/*   Updated: 2025/08/21 13:06:00 by arocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <sys/time.h>
 
-unsigned long get_time(void)
+unsigned long	get_time(void)
 {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (ts.tv_sec * 1000L) + (ts.tv_nsec / 1000000L);
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);	
+	return ((tv.tv_sec * 1000L) + (tv.tv_usec / 1000));
 }
-
-// unsigned long	get_time(void)
-// {
-// 	struct timeval	tv;
-
-// 	gettimeofday(&tv, NULL);
-// 	return ((tv.tv_sec * 1000L) + (tv.tv_usec / 1000));
-// }
 
 void	ms_wait(long ms)
 {
